acc5b6fd47c08acdf3e55ceb005d276f4bc30cc9
Cristian Fernandez <haansdev@gmail.com>
2022-02-27 04:36:03 +0100

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
